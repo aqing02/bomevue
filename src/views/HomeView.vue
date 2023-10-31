3d1b6d92453140d8560f236185c24d74e0a2291e
aqing <1134575174@qq.com>
2023-10-31 14:40:29 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: aqing 1134575174@qq.com
- * @Date: 2023-07-19 16:22:27
- * @LastEditors: aqing 1134575174@qq.com
- * @LastEditTime: 2023-10-27 14:56:21
- * @FilePath: \vue2_vite_ethers\src\views\HomeView.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div class="home-container">
     <Head title="首页"></Head>
